355304a04eb987f6e7f6ecf51982a6515321a198
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-07-30 10:22:45 +0200

    queue per scheduler

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -1,14 +1,33 @@
 import time 
 
-class Kernel:
+
+class Kernel: #kernel
+    pass
+
+class Memory: #allocazione memoria
+    def __init__(self,memory):
+        self.memory= memory
+
+    def allocate(self):
+        pass
+    
+    def free(self):
+        pass
+
+class scheduler: #gestione processi
     pass
 
 
-class Memory:
-    pass
+Q=[]
+def queue(x,maxSize=5): #first in first out
+    if maxSize <=0: return ' queue infinite'
+    if len(Q) < maxSize: Q.append(x)
+    else:
+        Q.insert(0,x)
+        Q.pop(-1)
+    return Q
 
-class scheduler:
-    pass
-
-
+for x in range(10):
+    print(queue(x,5))
+    # time.sleep(1)
 
