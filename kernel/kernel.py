3305553530ee61b85043519b1328df5fdd3026ae
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-07-30 17:28:06 +0200

    scheduler

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -1,50 +1,85 @@
 import time 
 
 class process:
-    pass
+    def __init__(self, name, memory_required):
+        self.name=name
+        self.memory_required=memory_required
 
-class Kernel: #kernel
-    pass
-
-class Memory: #allocazione memoria
-    def __init__(self,memory):
-        self.memory= memory
-        self.used_memory=0
-
-    def allocate(self,space):
-        self.used_memory += space
-        if self.used_memory <= self.memory: print(f'memoria allocata {space} memoria disponibile {  self.memory - (self.used_memory + space) }')
-        else: print(f'memoria allocata {self.used_memory} supera {space}')
+    def __str__(self):
+        return str(self.name)
+        
+    def run(self):
+        print(f'{self.name} terminated')      
     
-    def free(self, space):
-        self.used_memory -= space
-        print(f'memoria liberata di {space}')
-
 class scheduler: #gestione processi
-    def __init__(self,element, maxSize=5):
-        self.element=element
+    def __init__(self, n_process, maxSize=5):
+        self.n_process= n_process
         self.maxSize=maxSize
         self.Q=[]
-
+        
     def queue(self):
-        self.Q=self.element[:self.maxSize]
-        return ' initialize_queue '
+        n=0
+        if self.n_process <= self.maxSize: n = self.n_process
+        else : n = self.maxSize
+        self.Q=[process(x,200) for x in range(n)] #start queue
+        return self.Q
     
     def methods_queue(self):
         if self.maxSize <= 0: return 'infinite queue'
-        if len(self.element) < self.maxSize: # fewer elements compared to maxSize
+        if self.n_process < self.maxSize: # fewer elements compared to maxSize
             return self.Q
         else:     
-            for x in self.element[len(self.Q):]:
-                self.Q.append(x)
+            for x in  range(self.maxSize, self.n_process + 1): # methods n queue
+                y=process(x,200)
+                # print(y)
+                self.Q.append(y)
                 self.Q.pop(0)
+                # print(self.Q)
             return self.Q
+    
+    def __str__(self):
+       return str(self.Q)
+       for  x in self.Q:
+           print(x)
+     
+# class Kernel: #kernel
+#     def __init__(self, sheduler):
+#         self.memory= Memory(1024)
+#         self.scheduler=sheduler
+    
+#     def run(self):
+#         print(self.memory.allocate(200))
         
-M=Memory(1024)
-S=scheduler([1,2])
-print(S.queue())
-print(S.methods_queue())
-        
+# M=Memory(1024)
+
+# class Memory: #allocazione memoria
+#     def __init__(self,memory):
+#         self.memory= memory
+#         self.used_memory=0
+
+#     def allocate(self,space):
+#         self.used_memory += space
+#         if self.used_memory <= self.memory: print(f'memoria allocata {space} memoria disponibile {  self.memory - (self.used_memory + space) }')
+#         else: print(f'memoria allocata {self.used_memory} supera {space}')
+    
+#     def free(self, space):
+#         self.used_memory -= space
+#         print(f'memoria liberata di {space}')
+
+S=scheduler(20)
+S.queue()
+S.methods_queue()
+print(S)
+# K=Kernel(S)
+# K.run()
+# print(S.queue())
+# print(S.methods_queue())
+
+# for x in range(20):
+#     S=scheduler(x)
+#     S.element.run()
+    # print(S.queue())
+
 # Q = []
 
 # def queue(x,element, maxSize=5): # queue
