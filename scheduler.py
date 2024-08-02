7ebdea08280b16361cb534dffbfa0d5a34117a85
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-08-02 11:41:14 +0200

    graph

--- scheduler.py
+++ scheduler.py
@@ -10,9 +10,10 @@ from memory import Memory
 Burst_Time would be the time that the proccess need to be executes call BURST TIME
 '''
 sjf=False
+rr=False
 if len(sys.argv) > 2: raise Exception('add more than two parapeters')
-if len(sys.argv) == 2: 
-  sjf=True
+if sys.argv[1].upper() == 'SJF': sjf=True
+elif sys.argv[1].upper() == 'RR': rr= True
 class process:
     def __init__(self, name, memory_required, Burst_Time, arrival_time): 
         self.name=name
@@ -21,25 +22,14 @@ class process:
         self.remaining_time=Burst_Time
         self.arrival_time=arrival_time
         self.waiting_time=0
-        
-    def run(self):
-        # T=max(self.remaining_time,Quantum)
-        # T-=Quantum
-        # time.sleep(T * 0.1) # milliseconds
-        # if self.memory_required == 0 : cprint(f'finished execution: {self.name}','red')
-        # else : cprint(f'not finished: {self.name}, time left:{self.remaining_time * 0.1:.2f}s' ,'blue')
-        pass
 
-'''
- Quantum/time interval is the time the the scheduler give to all process 
- Shortest Job First chi ha il Burst_Time piu basso ha una priorità più alta 
-'''
 class scheduler: # gestione processi
     def __init__(self, n_process, maxSize=2):
         self.n_process= n_process
         self.maxSize=maxSize
         self.Q=[]
     
+    #TODO provare a sistemare questo se si riesce 
     def queue(self):
         if self.maxSize == 0: return 'infinite queue'
         elif len(self.n_process) > self.maxSize:
@@ -56,10 +46,10 @@ class scheduler: # gestione processi
         #     print(queue)
         # return queue  #preparation queue follow Algorithms ORR or SORR (the best)
     
-    # def __str__(self):
-    #    return str(self.Q)
 
-
+'''
+ Quantum/time interval is the time the the scheduler give to all process 
+'''
 class round_robin(scheduler):
     def __init__(self, n_process, maxSize=2, Quantum=5):
         super().__init__(n_process, maxSize)
@@ -68,36 +58,39 @@ class round_robin(scheduler):
     def run(self):
         complete_process=[]
         
-        while self.n_process != []: # methods n queue
+        while self.n_process != []:
             time.sleep(self.Quantum)
             self.n_process[0]['remaining_time'] -= self.Quantum
-            if self.n_process[0]['remaining_time'] <= 0: #rimozione processo dalla ueue
+            if self.n_process[0]['remaining_time'] <= 0: #remove process from queue 
                self.n_process[0]['remaining_time']=0
                print(f"process finished:{colored(self.n_process[0]['name'],'red')}")
                complete_process.append(self.n_process[0])
                self.n_process.pop(0)
-            else:
+            else: # put porcces at the end of the queue
                 print(f"not finished: {colored(self.n_process[0]['name'],'red')}   time left: {colored(self.n_process[0]['remaining_time'],'blue')}s ")
-                self.n_process= self.n_process[1:] + [self.n_process[0]] # elemento aggiunto alla coda della queue
+                self.n_process= self.n_process[1:] + [self.n_process[0]] 
 
         return complete_process
 
+'''
+Shortest Job First chi ha il Burst_Time piu basso ha una priorità più alta 
+'''
 class Shortest_Job_First(scheduler):
     def __init__(self, n_process, maxSize=2):
         super().__init__(n_process, maxSize)
     
-    def run(self):
+    def run(self): 
         L=len(self.n_process) - 1 
-        for y in range (L):
+        for y in range (L): #sort array
             for j in range(L - y):
                 if self.n_process[j]['Burst_Time'] > self.n_process[j + 1]['Burst_Time']:
                     self.n_process[j], self.n_process[j + 1] = self.n_process[j + 1] , self.n_process[j]
 
-        for x in self.n_process:
+        for x in self.n_process: # processes performed 
             time.sleep(x['Burst_Time'])
             print(f"process finished:{colored(x['name'],'red')}")
 
-    def graph(self):
+    def graph(self): #draw graph with burst time 
         run_time=[x['Burst_Time'] for x in self.n_process]
         n_process=[x['name'] for x in self.n_process]
         fig, ax = plt.subplots() 
@@ -108,8 +101,8 @@ class Shortest_Job_First(scheduler):
         plt.show()
 
 
-P=[process('ps1',256,5,0).__dict__,
-   process('ps2',256,9,2).__dict__,
+P=[process('ps1',256,1,0).__dict__,
+   process('ps2',256,1,2).__dict__,
    process('ps3',256,1,2).__dict__,
    process('ps4',256,5,0).__dict__,
    process('ps5',256,5,0).__dict__,
@@ -126,19 +119,16 @@ P=[process('ps1',256,5,0).__dict__,
 #     x.run(10)
 #     M.allocate(x.memory_required)
 
-maxSize=20
+maxSize=5
 S=scheduler(P,maxSize)
 Q=S.queue()
+if sjf:
+    SJF=Shortest_Job_First(Q,maxSize)
+    print(SJF.run())
+    SJF.graph()
+elif rr:
+    RR= round_robin(Q, maxSize, Quantum=6)
+    print(RR.run())
 
-RR= round_robin(Q, maxSize, Quantum=6)
-SJF=Shortest_Job_First(Q,maxSize)
-
-# print(SJF.run())
-SJF.graph()
-# print(RR.run())
-
-
-# if sjf: S.SJF()
-# print(S.round_robin())
 # Memory management in Python involves a private heap containing all Python objects and data structures.
 
